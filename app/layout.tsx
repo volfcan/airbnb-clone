e0254317cc8d98e863c34acaf16737d5e093ef86
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-03-19 10:43:56 +0300

    add auth ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Nunito } from 'next/font/google'
 
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly' 
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider';
 
 const font = Nunito({
   subsets: ["latin"],
@@ -21,7 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
